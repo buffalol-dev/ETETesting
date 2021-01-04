9bbd978e79addee18c2f18adf96ade5eb2f62bdf
natalie <nataliey6289@gmail.com>
2021-01-04 13:35:18 +0000

    http conn

--- ETETesting/UnitTest1.cs
+++ ETETesting/UnitTest1.cs
@@ -1,13 +1,50 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace ETETesting
 {
     [TestClass]
     public class UnitTest1
     {
+        static HttpClient client = new HttpClient();
+
         [TestMethod]
-        public void TestMethod1()
+        public async Task TestMethod1()
         {
+            var isin = "XS0085732716";
+            var dateFrom = new DateTime(2020,01,01);
+            var dateTo = new DateTime(2020, 01, 03);
+
+
+            await RunAsync(isin, dateFrom, dateTo);
+            
         }
+
+        static async Task RunAsync(string isin, DateTime dateFrom, DateTime dateTo)
+        {
+        
+            client.BaseAddress = new Uri("https://localhost:44325/");
+
+            var urlCheck = $"{client.BaseAddress}Returns/isin/?isin={isin}&valuationDate={dateTo}";
+
+            var response = await client.GetAsync($"{client.BaseAddress}Returns/isin/?isin={isin}&dateFrom={dateFrom}&dateTo={dateTo}");
+
+            var result = await response.Content.ReadAsAsync<ReturnsDTO>();
+
+            //assert
+        }
+
+
+        public class ReturnsDTO
+        {
+            public double DecimalReturn { get; set; }
+            public double PercentageReturn { get; set; }
+            public double BPSReturn { get; set; }
+        }
+
+
     }
 }
